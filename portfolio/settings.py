b0b0c0d77fcc66e9138e7063ab6a420641a93466
Vukasin <vukasinmladenovic1@gmail.com>
2022-11-27 21:47:46 +0100

    Deploy to Railway

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$_4go%hh-6wpu!%=!jddygtl!^kyjen*k5$qgq9#bu&k0f!x70'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DEBUG")) == 1
 
 ALLOWED_HOSTS = ['*']
 
